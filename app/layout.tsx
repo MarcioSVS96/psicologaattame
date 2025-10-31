5bc36716e2f19486dcea80aa918b095e75db0c1c
Márcio Santos <129344662+MarcioSVS96@users.noreply.github.com>
2025-10-31 06:26:47 -0300

    forma do google analitycs melhorada

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,24 +69,25 @@ export default function RootLayout({
             }),
           }}
         />
-        {/* Google tag (gtag.js) */}
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-GTHZ9BL3PR"
-        />
+      </head>
+      <body className={`font-sans ${inter.variable} ${playfair.variable} antialiased`}>
+        <Toaster richColors />
+        <Suspense fallback={null}>{children}</Suspense>
+        <Analytics />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtag/js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','G-GTHZ9BL3PR');
+
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-GTHZ9BL3PR');
           `}
         </Script>
-      </head>
-      <body className={`font-sans ${inter.variable} ${playfair.variable} antialiased`}>
-        <Toaster richColors />
-        <Suspense fallback={null}>{children}</Suspense>
-        <Analytics />
       </body>
     </html>
   );
