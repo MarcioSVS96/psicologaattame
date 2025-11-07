725e1573df3f68aec425781cb7c47d213484b60a
Márcio Santos <129344662+MarcioSVS96@users.noreply.github.com>
2025-11-07 16:18:43 -0300

    atualizando img hero

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -64,7 +64,7 @@ export function HeroSection() {
               style={{ backgroundColor: "rgba(64, 224, 208, 0.2)", borderColor: "rgba(64, 224, 208, 0.3)" }}
             >
               <img
-                src="/profile.png"
+                src="/profile.webp"
                 alt="Beatriz Attame - Psicóloga"
                 className="rounded-2xl object-cover w-full h-full"
               />
