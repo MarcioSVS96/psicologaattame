55044f3556c7f46b33edf924695291e757106d2f
Márcio Santos <129344662+MarcioSVS96@users.noreply.github.com>
2026-02-13 08:21:26 -0300

    corrigindo bug de agendamento

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,7 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!tea(?:/.*)?$|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 }
+
